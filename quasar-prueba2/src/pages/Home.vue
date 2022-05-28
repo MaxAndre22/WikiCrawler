30389edfb2341c685860711d25f809090cdf43cd
max <maximoandreaq@gmail.com>
2022-05-28 16:44:44 -0600

    ParteMax

---
+++ quasar-prueba2/src/pages/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-page class="flex column q-pa-md">
+    <span class="text-h1">Home</span>
+    <span class="text-h2">Home</span>
+    <span class="text-h3">Home</span>
+    <span class="text-h4">Home</span>
+    <q-separator />
+    <span class="text-h5">Home</span>
+    <span class="text-h6">Home</span>
+    <!--<h1>Home</h1> -->
+    <div class="q-pa-md q-gutter-sm">
+      <q-btn round color="black" icon="lab la-wikipedia-w" />
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "home-page",
+});
+</script>
