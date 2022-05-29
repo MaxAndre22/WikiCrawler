89a0b9cc2158a3b949fc411674c311aa0fd72dfa
max <maximoandreaq@gmail.com>
2022-05-28 19:44:40 -0600

    Boton home-wikipedia

--- quasar-prueba2/src/components/EssentialLink.vue
+++ quasar-prueba2/src/components/EssentialLink.vue
@@ -1,14 +1,6 @@
 <template>
-  <q-item
-    clickable
-    tag="a"
-    target="_blank"
-    :href="link"
-  >
-    <q-item-section
-      v-if="icon"
-      avatar
-    >
+  <q-item clickable tag="a" target="_blank" :href="link">
+    <q-item-section v-if="icon" avatar>
       <q-icon :name="icon" />
     </q-item-section>
 
@@ -17,33 +9,32 @@
       <q-item-label caption>{{ caption }}</q-item-label>
     </q-item-section>
   </q-item>
+
+    import { defineComponent } from "vue";
+
+    export default defineComponent({
+      name: "EssentialLink",
+      props: {
+        title: {
+          type: String,
+          required: true,
+        },
+
+        caption: {
+          type: String,
+          default: "",
+        },
+
+        link: {
+          type: String,
+          default: "#",
+        },
+
+        icon: {
+          type: String,
+          default: "",
+        },
+      },
+    });
+  </script>
 </template>
-
-<script>
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  name: 'EssentialLink',
-  props: {
-    title: {
-      type: String,
-      required: true
-    },
-
-    caption: {
-      type: String,
-      default: ''
-    },
-
-    link: {
-      type: String,
-      default: '#'
-    },
-
-    icon: {
-      type: String,
-      default: ''
-    }
-  }
-})
-</script>
--- quasar-prueba2/src/layouts/MainLayout.vue
+++ quasar-prueba2/src/layouts/MainLayout.vue
@@ -1,27 +1,35 @@
 <template>
   <q-layout view="lHh Lpr lFf" class="bg-primary">
     <div className="Logo" position="top" class="flex flex-center" padding>
-      <q-img
-        src="~/assets/wikipedialogo.png"
-        style="width: 150px; height: 150px; margin-right:2%;"
+      <q-btn
+        icon="img:/icons/wikipedialogo.png"
+        size="70px"
+        flat
+        round
+        dense
+        @click="$router.replace('home')"
       />
-      <h1 style="color:white; font-family: 'Inter', sans-serif;">WikiCrawler</h1>
+      <h1 style="color: white; font-family: 'Inter', sans-serif">
+        WikiCrawler
+      </h1>
     </div>
-    
-    <q-page-container >
-      <router-view/>
+
+    <q-page-container>
+      <router-view />
     </q-page-container>
   </q-layout>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'MainLayout',
+  name: "MainLayout",
 
-  setup () {
-    return {}
-  }
-})
+  setup() {
+    return {};
+  },
+});
 </script>
+
+<style></style>
